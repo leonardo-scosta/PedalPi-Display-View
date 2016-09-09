8a48a14553c341dce9068652f3b11466a5259e14
p4x3c0 <leonardo_alamania_12@hotmail.com>
2016-09-09 13:37:20 -0300

    Telas Feitas

--- app/src/main/java/com/pedalpi/pedalpi/Main3Activity.java
+++ app/src/main/java/com/pedalpi/pedalpi/Main3Activity.java
@@ -4,18 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.pedalpi.pedalpi.componente.RotaryKnob;
 
 
 public class Main3Activity extends AppCompatActivity {
 
     Spinner parametro1;
-
     ToggleButton toggleButton1;
+    SeekBar seekBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,6 @@ public class Main3Activity extends AppCompatActivity {
         parametro1 = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.Parâmetro_1,android.R.layout.simple_spinner_dropdown_item);
         parametro1.setAdapter(adapter);
-
-
-        RotaryKnob knob = (RotaryKnob) findViewById(R.id.knob);
-        knob.setImageResource(R.drawable.botao);
 
 
         //parametro 1 - plugin 1
@@ -46,5 +44,42 @@ public class Main3Activity extends AppCompatActivity {
             }
         });
         //parametro 2 - plugin 1
+        seekbar();
     }
+    //parametro 3 - plugin 1
+    public void seekbar(){
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(seekBar.getProgress() + "/" +seekBar.getMax());
+
+
+
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener(){
+                    //double progress_value;
+                    int progress_value;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        //progress_value = ((double) progress);
+                        progress_value = progress;
+                        //textView.setText(progress/100.0 + "/" +seekBar.getMax()/100);
+                        textView.setText(progress + "/" +seekBar.getMax());
+
+                        //Toast.makeText(Main3Activity.this,"Parametro alterado!",Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        //textView.setText(progress_value/100.0 + "/" +seekBar.getMax()/100);
+                        textView.setText(progress_value + "/" +seekBar.getMax());
+                    }
+                }
+
+        );
+    }
+    //parametro 3 - plugin 1
+
 }
